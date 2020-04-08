f5422da7ff066d51e0fd17900de52a507afcaa0e
koenr <koen@roggemans.net>
2020-04-08 10:53:08 +0200

    initial commit

---
+++ bbb_monitor.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# script to monitor BBB server
+# https://groups.google.com/forum/#!msg/bigbluebutton-dev/Et2D56iweT0/gFsngKPiEAAJ
+# execute this with a cron job. Log file must be in a web accessible place
+# Koen Roggemans 2020/04/08
+ 
+datum=$(date +"%F %R")
+users=$(mongo --quiet mongodb://127.0.1.1:27017/meteor --eval "db.users.count({connectionStatus: 'online'})")
+meetings=$(mongo --quiet mongodb://127.0.1.1:27017/meteor --eval "db.meetings.find()" | grep "meetingEnded\" : false," | wc -l)
+audio=$(/opt/freeswitch/bin/fs_cli -x "show channels" | grep total | sed 's/ total.//')
+
+echo "\n$datum,$users,$meetings,$audio" >> /var/www/bbbmonitor/bbb_usage.log
